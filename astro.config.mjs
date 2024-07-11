91a3b6d037b96484cd389e33c97c8803658c6788
Yuriy Grinev <a1d516ac5f5d290@gmail.com>
2024-07-11 15:31:28 +0300

    Попытка не пытка

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vue-norma.ru',
   devToolbar: {
     enabled: false
   }
